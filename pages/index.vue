6b4fd6e014dd7a4373e9968dded5fc478d5dd2c2
Tilen Pirih <tilen.pirih@gmail.com>
2024-08-21 14:27:15 +0200

    feat: Add lazy loading for project images and improve project card layout

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,20 @@ const projects = [
     lazyImage: '/img/lazy/globalPDR.webp',
     link: 'https://google.com',
   },
+  {
+    title: 'Global PDR Application',
+    description: 'Description 1 asdf asd fasd fasd fasdf  asdfa sdfas dfa sdf asdf asdf asdf asd fas dfasd fasd asd fasd fas dfasd fasd fasd fasd fasd f',
+    image: '/img/globalPDR.webp',
+    lazyImage: '/img/lazy/globalPDR.webp',
+    link: 'https://google.com',
+  },
+  {
+    title: 'Global PDR Application',
+    description: 'Description 1',
+    image: '/img/globalPDR.webp',
+    lazyImage: '/img/lazy/globalPDR.webp',
+    link: 'https://google.com',
+  },
 ]
 </script>
 
@@ -51,11 +65,14 @@ const projects = [
     </div>
     <v-row>
       <v-col v-for="(project, index) in projects" :key="index" cols="12" sm="6" lg="4">
-        <v-card class="projectCard">
+        <v-card class="projectCard h-100">
           <v-img :src="project.image" :lazy-src="project.lazyImage" aspect-ratio="1.777" />
           <v-card-title class="text-h5 text-primary">
             {{ project.title }}
           </v-card-title>
+          <v-card-text>
+            {{ project.description }}
+          </v-card-text>
         </v-card>
       </v-col>
     </v-row>

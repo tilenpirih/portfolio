5c690fc51540a92ab48cea886a07643171c0131e
Tilen Pirih <tilen.pirih@gmail.com>
2024-08-28 20:52:04 +0200

    added OGImage

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
+const runtimeConfig = useRuntimeConfig()
 useHead({
   title: 'Tilen Pirih - Fullstack developer',
   meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
     { name: 'description', content: 'My portfolio' },
+    { name: 'keywords', content: 'portfolio, developer, fullstack, frontend, backend, Tilen, Pirih' },
+    { name: 'author', content: 'Tilen Pirih' },
+    { property: 'og:title', content: 'Tilen Pirih - Fullstack developer' },
+    { property: 'og:image', content: `${runtimeConfig.public.BASE_URL}/img/OGImage.webp` },
+    { property: 'og:type', content: 'website' },
+    { property: 'og:url', content: `${runtimeConfig.public.BASE_URL}` },
   ],
 })
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,9 @@ export default defineNuxtConfig({
     EMAILJS_PRIVATE_KEY: process.env.EMAILJS_PRIVATE_KEY,
     EMAILJS_SERVICE_ID: process.env.EMAILJS_SERVICE_ID,
     EMAILJS_TEMPLATE_ID: process.env.EMAILJS_TEMPLATE_ID,
+    public: {
+      BASE_URL: process.env.BASE_URL,
+    },
   },
   build: {
     transpile: ['vuetify'],

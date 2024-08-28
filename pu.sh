f838bae551af09d4560de131f0479f5844fb336a
Tilen Pirih <tilen.pirih@gmail.com>
2024-08-28 18:04:40 +0200

    chore: Update deployment script to handle repository names with slashes

--- pu.sh
+++ pu.sh
@@ -9,7 +9,7 @@ docker build -t registry.ventic.tv/$REPOSITORY_NAME/$PROJECT_NAME:$TAG .
 docker push registry.ventic.tv/$REPOSITORY_NAME/$PROJECT_NAME:$TAG
 if [ $TAG = "staging" ]
 then
-  kubectl rollout restart deployment/$PROJECT_NAME-staging -n $REPOSITORY_NAME
+  kubectl rollout restart deployment/$PROJECT_NAME-staging -n ${REPOSITORY_NAME/\//-}
 else
-  kubectl rollout restart deployment/$PROJECT_NAME -n $REPOSITORY_NAME
+  kubectl rollout restart deployment/$PROJECT_NAME -n ${REPOSITORY_NAME/\//-}
 fi

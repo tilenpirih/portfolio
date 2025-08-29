d74e3e5358e2fc71af640f3121fa64bc64d34fd7
Tilen Pirih <tilen.pirih@gmail.com>
2025-08-29 16:50:31 +0200

    Migrated all projects

--- app/components/customFooter.vue
+++ app/components/customFooter.vue
@@ -16,14 +16,14 @@ const discordIcon = 'm22 24l-5.25-5l.63 2H4.5A2.5 2.5 0 0 1 2 18.5v-15A2.5 2.5 0
         <v-btn class="mx-1" :icon="mdiFacebook" variant="outlined" href="https://www.facebook.com/tilen.pirih/" target="_blank" />
       </div>
       <div class="d-flex justify-center mt-3">
-        <v-icon class="mr-2" :icon="mdiPhone" />(+386) 51 731 371
+        <v-icon class="mr-2" color="primary" :icon="mdiPhone" />(+386) 51 731 371
       </div>
       <div class="d-flex justify-center">
-        <v-icon class="mr-2" :icon="mdiEmail" /> tilen.pirih@gmail.com
+        <v-icon class="mr-2" color="primary" :icon="mdiEmail" /> tilen.pirih@gmail.com
       </div>
       <div class="d-flex justify-center align-center mt-2">
         Tilen Pirih
-        <v-icon :icon="mdiCopyright" size="small" class="ml-2" />2024
+        <v-icon :icon="mdiCopyright" size="small" class="ml-1" />2024
       </div>
     </div>
   </v-footer>
---
+++ app/data/projects/nktolmin.ts
@@ -0,0 +1,55 @@
+import type { ProjectData } from '~/types/main'
+
+export default {
+  title: 'NK Tolmin',
+  description: 'This project was particularly meaningful to me because it involved revamping the website of my local football club, where I had played for 10 years. Additionally, I saw an opportunity to incorporate this work into my college <a href="https://repozitorij.uni-lj.si/IzpisGradiva.php?id=145003&lang=slv" target="_blank">thesis</a>. My goal was to make it easier for the club to manage their online presence, so I developed a web scraper that automatically pulled relevant public data and displayed it seamlessly on the website.<br>Unfortunately, the project had to be abandoned. The club chose not to compensate me adequately, and without proper pay, I lacked the time and motivation to continue working and maintaining the project.',
+  githubUrl: 'https://github.com/tilenpirih?tab=repositories&q=nk-tolmin&type=&language=&sort=',
+  image: '/img/nktolmin.webp',
+  lazyImage: '/img/lazy/nktolmin.webp',
+  technologies: [
+    {
+      icon: '/img/technologies/vue.svg',
+      link: 'https://vuejs.org/',
+    },
+    {
+      icon: '/img/technologies/vuetify.svg',
+      link: 'https://vuetifyjs.com/',
+    },
+    {
+      icon: '/img/technologies/vuex.svg',
+      link: 'https://vuex.vuejs.org/',
+    },
+    {
+      icon: '/img/technologies/js.svg',
+      link: 'https://www.w3schools.com/js/',
+    },
+    {
+      icon: '/img/technologies/nodejs.svg',
+      link: 'https://nodejs.org/en',
+    },
+    {
+      icon: '/img/technologies/flask.svg',
+      link: 'https://flask.palletsprojects.com/',
+    },
+    {
+      icon: '/img/technologies/docker.svg',
+      link: 'https://www.docker.com/',
+    },
+    {
+      icon: '/img/technologies/peewee.webp',
+      link: 'https://docs.peewee-orm.com/',
+    },
+    {
+      icon: '/img/technologies/beautifulSoup.png',
+      link: 'https://beautiful-soup-4.readthedocs.io/',
+    },
+  ],
+  seo: {
+    title: 'Project NK Tolmin',
+    description: 'This project was particularly meaningful to me because it involved revamping the website of my local football club, where I had played for 10 years. My goal was to make it easier for the club to manage their online presence, so I developed a web scraper that automatically pulled relevant public data and displayed it seamlessly on the website.',
+    ogTitle: 'Project NK Tolmin',
+    ogImage: '/img/nktolmin.webp',
+    ogUrl: '/project/nktolmin',
+  },
+  videoId: '_NxHsBEe0xY',
+} satisfies ProjectData
